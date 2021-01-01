37705413652243083c06afd5d758e68c2370968e
Varmo Ernits <varmoe@gmail.com>
2021-01-02 00:44:38 +0100

    added name input

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void btnClick(View view) {
+        TextView txtHello = findViewById(R.id.txtMessage);
+
+        EditText edtTxtName = findViewById(R.id.Namefield);
+        txtHello.setText("Hello " + edtTxtName.getText().toString());
     }
 }
